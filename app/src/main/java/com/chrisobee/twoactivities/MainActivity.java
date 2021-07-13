990777d490893c79497e8b421e6cd03a1bbfd4df
Chris O'Brien <chrisobrien2000@outlook.com>
2021-07-13 13:39:47 -0500

    Added a Log every time the 'Send' button is clicked

--- app/src/main/java/com/chrisobee/twoactivities/MainActivity.java
+++ app/src/main/java/com/chrisobee/twoactivities/MainActivity.java
@@ -3,9 +3,12 @@ package com.chrisobee.twoactivities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +17,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
     }
 }
